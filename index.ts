a06bc113d0424f7e7c11884248a1f1bb4a13be8f
kodegoph <82753202+kodegoph@users.noreply.github.com>
2021-06-01 17:21:39 +0800

    update

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
+app.use("/public", express.static(path.join(__dirname, "dist")));
+console.log(path.join(__dirname, "dist"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
