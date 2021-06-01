b3dbf871541afd9b5f9c2b107f34d89e7da5e9c1
kodegoph <82753202+kodegoph@users.noreply.github.com>
2021-06-01 17:06:08 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,75 @@
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  mode: "development",
+  // Define the entry points of our application (can be multiple for different sections of a website)
+  entry: {
+    main: "./src/js/main.js",
+  },
+
+  // Define the destination directory and filenames of compiled resources
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+
+  // Define development options
+  devtool: "source-map",
+
+  // Define loaders
   module: {
     rules: [
+      // Use babel for JS files
       {
-        test: /\.(scss)$/,
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
         use: [
+          MiniCssExtractPlugin.loader,
           {
-            // inject CSS to page
-            loader: "style-loader",
-          },
-          {
-            // translates CSS into CommonJS modules
             loader: "css-loader",
-          },
-          {
-            // Run postcss actions
-            loader: "postcss-loader",
             options: {
-              // `postcssOptions` is needed for postcss 8.x;
-              // if you use postcss 7.x skip the key
-              postcssOptions: {
-                // postcss plugins, can be exported to postcss.config.js
-                plugins: function () {
-                  return [require("autoprefixer")];
-                },
-              },
+              importLoaders: 2,
+              sourceMap: true,
+              url: false,
             },
           },
           {
-            // compiles Sass to CSS
-            loader: "sass-loader",
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: ["autoprefixer"],
+              },
+            },
           },
+          "sass-loader",
         ],
       },
     ],
   },
+
+  // Define used plugins
+  plugins: [
+    // Load .env file for environment variables in JS
+    new Dotenv({
+      path: "./.env",
+    }),
+
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
