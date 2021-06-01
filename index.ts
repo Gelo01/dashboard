32aa280c504b3152851ae23d26e1c1d94eaad951
kodegoph <82753202+kodegoph@users.noreply.github.com>
2021-06-01 15:10:27 +0800

    pass value to view

--- index.ts
+++ index.ts
@@ -10,8 +10,21 @@ const PORT = process.env.APP_PORT || 5000;
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("pages/index"));
-app.get("/login", (req, res) => res.render("pages/login"));
+app.get("/", (req, res) => {
+  let tagline: { msg: string } = {
+    msg: "No programming concept is complete without a cute animal mascot.",
+  };
+
+  res.render("pages/index", tagline);
+});
+
+app.get("/login", (req, res) => {
+  let tagline: { msg: string } = {
+    msg: "No programming concept is complete without a cute animal mascot.",
+  };
+
+  res.render("pages/login", tagline);
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
