f72de8e0652dc9d8481e6015346e3e3c7d7f136c
kodegoph <82753202+kodegoph@users.noreply.github.com>
2021-06-01 14:56:46 +0800

    added login page

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
 
 dotenv.config({
   path: ".env",
@@ -12,6 +11,7 @@ const PORT = process.env.APP_PORT || 5000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("pages/index"));
+app.get("/login", (req, res) => res.render("pages/login"));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
